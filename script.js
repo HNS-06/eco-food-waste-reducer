55e29aabbb3c738dc8bfd920be3d90a3c819ba01
czx_neo <136159343+czx-neo@users.noreply.github.com>
2025-02-15 01:17:57 +0530

    deleted server js and moved server to index.html

--- script.js
+++ script.js
@@ -87,15 +87,24 @@ function displayRecipes(recipes) {
     });
 
 }
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('ServiceWorker registration successful');
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/script.js',
+                // Add other assets to cache
+            ]);
         })
-        .catch(err => {
-          console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-  }
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
