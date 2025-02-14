4751563d5a84aeac0bbf9f8aa41805b5e672ada3
czx_neo <136159343+czx-neo@users.noreply.github.com>
2025-02-15 00:44:21 +0530

    web app beta

--- script.js
+++ script.js
@@ -85,4 +85,17 @@ function displayRecipes(recipes) {
         `;
         recipesDiv.appendChild(recipeElement);
     });
+
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('ServiceWorker registration successful');
+        })
+        .catch(err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+  }
